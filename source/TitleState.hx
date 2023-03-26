cb1d01e02e7dc64cb4ece0cacc0d754ec841cd09
Mini Raposa Gamer <115371844+MiniRaposaGAMER@users.noreply.github.com>
2023-03-26 17:25:28 -0300

    cu

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -496,7 +496,7 @@ class TitleState extends MusicBeatState
 					if (mustUpdate) {
 						MusicBeatState.switchState(new OutdatedState());
 					} else {
-						MusicBeatState.switchState(new MainMenuState());
+						MusicBeatState.switchState(new FreeplayState());
 					}
 					closedState = true;
 				});
